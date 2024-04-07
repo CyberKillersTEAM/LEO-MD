ca98823823c19f8e744a1760a1473ffa09d5573d
Mʀ Iꜱᴜʀᴜ X <156693573+MrIsuru@users.noreply.github.com>
2024-04-07 18:40:32 +0530

    ＢＬＯＯＤＹ  ＳＷＥＥＴ !

---
+++ commands/ping.js
@@ -0,0 +1,29 @@
+/**
+
+Copyright (C) 2024.        
+
+ * @project_name : LEO-MD    
+ 
+ * @authors : CyberKillersTEAM <https://github.com/CyberKillersTEAM>   
+ 
+ * @description : LEO-MD is simple bot by CyberKillersTEAM.     
+   
+ * @version 1.0.0   
+ 
+ **/
+ 
+const Secktor = require('../lib')
+Secktor.cmd({
+        pattern: "ping",
+        desc: "To check ping",
+        category: "general",
+        filename: __filename,
+    },
+    async(Void, citel) => {
+        var inital = new Date().getTime();
+        const { key } = await Void.sendMessage(citel.chat, {text: '```Testing Ping```'});
+        var final = new Date().getTime();
+       // await Secktor.sleep(1000)
+       return await Void.sendMessage(citel.chat, {text: '*🎈Pɪɴɢ : ' + (final - inital) + ' Mꜱ* \n\n*⦁ ʟᴇᴏ - ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ ⦁*', edit: key});
+    }
+);
